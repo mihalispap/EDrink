e8458b0e656e0f203394141510c54b4b983779f3
MihalisP <mihalispapak@gmail.com>
2020-11-07 04:23:10 +0200

    added settings model

--- edrink/models/__init__.py
+++ edrink/models/__init__.py
@@ -1,6 +1,6 @@
-
-__all__ = ['Avatar', 'Participant', 'Room']
+__all__ = ['Avatar', 'Participant', 'Room', 'Settings']
 
 from edrink.models.avatar import Avatar
 from edrink.models.participant import Participant
 from edrink.models.room import Room
+from edrink.models.settings import Settings
